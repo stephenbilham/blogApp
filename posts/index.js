fd1dcc9ef7badac3614f8a2838d4903a0cf857a3
stephen-bilham-tm <stephenbilham@yahoo.com>
2023-12-04 14:04:37 -0800

    added event bus endpoint and gitignore for node modules

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -8,11 +9,15 @@ app.use(cors());
 
 const posts = {};
 
+// get endpoint
 app.get("/posts", (req, res) => {
 	res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+// post endpoint
+app.post("/posts", async (req, res) => {
+	console.log("POST /posts endpoint hit");
+
 	const id = randomBytes(4).toString("hex");
 	const { title } = req.body;
 
@@ -21,9 +26,29 @@ app.post("/posts", (req, res) => {
 		title,
 	};
 
-	res.status(201).send(posts[id]);
+	try {
+		await axios.post("http://localhost:4005/events", {
+			type: "postCreated",
+			data: {
+				id,
+				title,
+			},
+		});
+		res.status(201).send(posts[id]);
+	} catch (error) {
+		console.error("Error sending event:", error);
+		res.status(500).send({ error: "Internal Server Error" });
+	}
 });
 
+// post to event bus
+app.post("/events", (req, res) => {
+	console.log("Event Recieved", req.body.type);
+
+	res.send({});
+});
+
+// port
 app.listen(4000, () => {
 	console.log("listening on port 4000");
 });

6c15b74f0e2da91626d0862aa7b090c4218b04b2
stephen-bilham-tm <stephenbilham@yahoo.com>
2023-12-05 12:36:32 -0800

    set up the ui to reject comments and also add events to query stack so if it goes down it'll catch up on reload

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,10 +1,21 @@
 import React from "react";
 
 const CommentList = ({ comments }) => {
+	const getContentForComment = (comment) => {
+		switch (comment.status) {
+			case "approved":
+				return comment.content;
+			case "rejected":
+				return "This comment has been rejected";
+			default:
+				return "This comment is pending";
+		}
+	};
+
 	const renderedComments = comments ? (
-		comments.map((comment) => {
-			return <li key={comment.id}>{comment.content}</li>;
-		})
+		comments.map((comment) => (
+			<li key={comment.id}>{getContentForComment(comment)}</li>
+		))
 	) : (
 		<div />
 	);
--- query/index.js
+++ query/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
@@ -8,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-	res.send(posts);
-});
-
-app.post("/events", (req, res) => {
-	const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
 	if (type === "PostCreated") {
 		const { id, title } = data;
 
@@ -41,18 +36,35 @@ app.post("/events", (req, res) => {
 		const { id, content, status, postId } = data;
 
 		const post = posts[postId];
-
 		const comment = post.comments.find((comment) => comment.id === id);
 
 		comment.status = status;
 		comment.content = content;
 	}
+};
+
+app.get("/posts", (req, res) => {
+	res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+	const { type, data } = req.body;
+	handleEvent(type, data);
 
 	res.send({});
 });
 
 const PORT = 4002;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
 	console.log(`listening on ${PORT}`);
+	try {
+		const res = await axios.get("http://localhost:4005/events");
+		for (const event of res.data) {
+			console.log("proccessing event:", event.type);
+			handleEvent(event.type, event.data);
+		}
+	} catch (error) {
+		console.log(error);
+	}
 });
